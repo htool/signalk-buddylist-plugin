700ba9ca52ca3935926cdb895af5a8abccc84e68
Hans Tool <hans@defect.org>
2021-02-18 08:47:55 +0100

    bla

--- index.js
+++ index.js
@@ -111,10 +111,7 @@ module.exports = function(app) {
         delta.updates.forEach(update => {
           update.values.forEach(pv => {
             if ( pv.path == 'navigation.position' ) {
-              app.debug('buddy.name: ' + buddy.name)
-              if (typeof buddy.name != 'undefined') {
-                checkBuddy(buddy.urn, buddy.name, props.alert, props.alertDistance, pv.value)
-              }
+              checkBuddy(buddy.urn, buddy.name, props.alert, props.alertDistance, pv.value)
             }
           })
         })
